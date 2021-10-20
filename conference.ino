cd36cd4e001fdab2b03ef81a62dc4d85c3bd7e15
dph5087 <dhoysan@andrew.cmu.edu>
2021-10-19 21:08:04 -0400

    Update conference.ino

--- conference.ino
+++ conference.ino
@@ -1,42 +1,64 @@
 /*****************************************************************************
-Particle Maker Kit Tutorial #3: PIR Motion Sensor
+Revised Particle Maker Kit Tutorial #3: PIR Motion Sensor
 
-This tutorial uses a Photon and the PIR motion sensor from the Particle Maker
-Kit to determine whether a conference room is in use (you could also use it
+This tutorial uses a Particle Argon and PIR motion sensor
+to determine whether a conference room is in use (you could also use it
 for many other applications) and post the status to Slack.
 ******************************************************************************/
 
-int ledPin = D7;                 // choose the pin for the LED
-int inputPin = D2;               // choose the PIR sensor pin
-String status = "available to start the day";          // string to hold "available" or "in use",
-int pirState = 0;
-int currentState = LOW; 
+// Define your variables
+int ledPin          =   D7;     // Choose the D7 pin for the LED
+int inputPi         =   D2;     // Choose the D2 pin for the PIR sensor
+int pirState        =   LOW;    // Create a variable to capture the PIR state when read from the D2 pin.
+int currentState    =   LOW;    // This variable will be compared to the pirState varible to determine the status of the conference room. Set it LOW to start.
 
-void setup() {
+// Create a string to hold the status of the conference room. 
+// This status will eventually switch between "available" or "in use" depending on the status of the PIR sensor
+String status = "available to start the day";
+
+// Setup loop
+void setup() 
+{
     pinMode(ledPin, OUTPUT);       // set LED as output
-    pinMode(inputPin, INPUT);      // set sensor as input
-    Particle.publish("conf_avail", status, PRIVATE); // send test publish
-    Serial.begin(9600);
+    pinMode(inputPin, INPUT);      // set PIR sensor as input
+    
+    // Use the particle publish function to send the initial status of the conference room to slack
+    // If successful, this will tell you the conference room is "available to start the day" on slack
+    Particle.publish("conf_avail", status, PRIVATE); 
+    
+    // Start the serial monitor for troubleshooting. 
+    Serial.begin(9600); // This line is optional and can be deleted if not needed. 
 }
 
-
+// Start the loop
 void loop() 
 {
+    //Read the PIR sensor and hold the value in the pirState variable. 
     pirState = digitalRead(inputPin);
-    Serial.printlnf("PIR State %d", pirState);
+    
+    // Print the PIR State to serial for troubleshooting purposes.
+    Serial.printlnf("PIR State %d", pirState);  // This line is optional and can be deleted if not needed
 
-    if (currentState == LOW && pirState == HIGH) {  // check if the input is HIGH
-        digitalWrite(ledPin, HIGH);         // turn LED ON if high
-        status = "in use";  
-        Particle.publish("conf_avail", status, PRIVATE); //publish to conf_avail webhook
-        currentState = HIGH;
+    // Compare the current State to the pirState.
+    // If currentState is LOW and pirState is HIGH, this means someone entered a vacant conference room.
+    if (currentState == LOW && pirState == HIGH) 
+    {  
+        digitalWrite(ledPin, HIGH); // turn LED ON to signify the room is in use
+        status = "in use"; // Set the conference room status to "in use" 
+        Particle.publish("conf_avail", status, PRIVATE); //publish to conf_avail webhook. This will send the new status to Slack
+        currentState = HIGH; // Set the currentState to HIGH since the room is now in use.
     } 
-    if (currentState == HIGH && pirState == LOW) {
-        digitalWrite(ledPin, LOW);          // turn LED OFF if no input
-        status = "available";
-        Particle.publish("conf_avail", status, PRIVATE); //publish to conf_inuse webhook
-        currentState = LOW; 
+    
+    // Compare the current State to the pirState.
+    // If currentState is HIGH and pirState is LOW, this means the conference room is empty again.
+    if (currentState == HIGH && pirState == LOW) 
+    {
+        digitalWrite(ledPin, LOW);          // turn LED OFF to signify the room is empty
+        status = "available"; // Set the conference room status to "available"
+        Particle.publish("conf_avail", status, PRIVATE); //publish to conf_avail webhook. This will send the new status to Slack
+        currentState = LOW; //Set the currentState to LOW since the room is now empty.
     }
 
+    // 2 second delay in between PIR readings.
     delay(2000);
 }
